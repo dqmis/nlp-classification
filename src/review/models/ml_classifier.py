e42c1da51150abd718584e8c02957286a20c0304
Dominykas Šeputis <dom.seputis@gmail.com>
2023-04-19 19:17:29 +0300

    Updated classifyer

--- src/review/models/ml_classifier.py
+++ src/review/models/ml_classifier.py
@@ -1,7 +1,9 @@
-from typing import List
+from typing import Any, Dict, List, Optional
 
+import eli5
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.pipeline import Pipeline
 from xgboost import XGBClassifier
@@ -9,21 +11,82 @@ from xgboost import XGBClassifier
 from src.review.models.components.stemmer import Stemmer
 
 _classifiers = {
-    "logreg": LogisticRegression(),
-    "xgboost": XGBClassifier(),
-    "nb": BernoulliNB(),
+    "logreg": LogisticRegression,
+    "xgboost": XGBClassifier,
+    "nb": BernoulliNB,
 }
 
 
+def _parse_search_to_model_params(params: Dict[str, Any]) -> Dict[str, Any]:
+    filtered_dict: Dict[str, Any] = {}
+    for key, value in params.items():
+        if "classifyer__" in key:
+            filtered_dict[key.split("classifyer__")[-1]] = value
+    return filtered_dict
+
+
+def _parse_model_params_to_search(params: Dict[str, Any]) -> Dict[str, Any]:
+    filtered_dict: Dict[str, Any] = {}
+    for key, value in params.items():
+        filtered_dict["classifyer__" + key] = value
+    return filtered_dict
+
+
 class MlClassifier:
-    def __init__(self, language: str = "english", classifier: str = "logreg"):
+    def __init__(self, language: str = "english", classifier_name: str = "logreg"):
         self.language = language
-        self.pipeline = Pipeline(
+        self.classifier_name = classifier_name
+        self.pipeline = self._define_pipeline()
+
+    def hyperparam_search(
+        self,
+        X: List[str],
+        y: List[int],
+        search_params: Dict[str, Any],
+        n_iter: int = 100,
+        cv: int = 5,
+        random_state: int = 42,
+    ) -> None:
+        """
+        Search for the best parameters for the classifier.
+        To pass search_params, define them with prefix "classifyer__" and
+        pass them as a dictionary.
+        """
+
+        search = RandomizedSearchCV(
+            self.pipeline,
+            _parse_model_params_to_search(search_params),
+            n_iter=n_iter,
+            cv=cv,
+            random_state=random_state,
+        )
+        search.fit(X, y)
+
+        self.pipeline = self._define_pipeline(
+            _parse_search_to_model_params(search.best_estimator_.get_params())
+        )
+
+    def _define_pipeline(
+        self,
+        classifier_params: Optional[Dict[str, Any]] = None,
+    ) -> Pipeline:
+        if classifier_params is None:
+            classifier = _classifiers[self.classifier_name]()
+        else:
+            classifier = _classifiers[self.classifier_name](**classifier_params)
+        return Pipeline(
             [
                 ("stemmer", Stemmer()),
-                ("cv", CountVectorizer(stop_words=language)),
-                ("classifyer", _classifiers[classifier]),
+                ("cv", CountVectorizer(stop_words=self.language)),
+                ("classifyer", classifier),
             ]
+        )
+
+    def get_feature_importance(self, top: int = 30) -> eli5.base.Explanation:
+        return eli5.explain_weights(
+            self.pipeline.named_steps["classifyer"],
+            top=top,
+            feature_names=self.pipeline.named_steps["cv"].get_feature_names_out(),
         )
 
     def fit(self, X: List[str], y: List[int]) -> None:
